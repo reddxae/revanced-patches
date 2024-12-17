512b2526ba1b4b2c5d74d5b51f1679814ec4065e
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-12-17 14:35:06 +0900

    chore: Lint code

--- patches/src/main/kotlin/app/revanced/patches/music/player/components/PlayerComponentsPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/music/player/components/PlayerComponentsPatch.kt
@@ -823,7 +823,7 @@ val playerComponentsPatch = bytecodePatch(
         // region patch for remember repeat state
 
         val (repeatTrackMethod, repeatTrackIndex) = repeatTrackFingerprint.matchOrThrow().let {
-            with (it.method) {
+            with(it.method) {
                 val targetIndex = it.patternMatch!!.endIndex
                 val targetRegister = getInstruction<OneRegisterInstruction>(targetIndex).registerA
 

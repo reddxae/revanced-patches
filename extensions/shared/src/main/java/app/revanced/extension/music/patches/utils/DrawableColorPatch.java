d748b6d12f4e84cff67d1aa31c07be53fc6d4995
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-01-06 21:33:17 +0900

    feat(YouTube Music): Add `Dark theme` patch, Remove `Amoled` patch

--- extensions/shared/src/main/java/app/revanced/extension/music/patches/utils/DrawableColorPatch.java
+++ extensions/shared/src/main/java/app/revanced/extension/music/patches/utils/DrawableColorPatch.java
@@ -1,21 +1,44 @@
 package app.revanced.extension.music.patches.utils;
 
+import android.graphics.drawable.Drawable;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import app.revanced.extension.shared.utils.ResourceUtils;
+import app.revanced.extension.shared.utils.Utils;
+
 @SuppressWarnings("unused")
 public class DrawableColorPatch {
     private static final int[] DARK_VALUES = {
-            -14606047 // comments box background
+            -14606047, // comments box background
+            -16579837, // button container background in album
+            -16777216, // button container background in playlist
     };
 
-    public static int getLithoColor(int originalValue) {
-        if (anyEquals(originalValue, DARK_VALUES))
-            return -16777215;
+    // background colors
+    private static final Drawable headerGradient =
+            ResourceUtils.getDrawable("revanced_header_gradient");
+    private static final int blackColor =
+            ResourceUtils.getColor("yt_black1");
 
-        return originalValue;
+    public static int getLithoColor(int originalValue) {
+        return ArrayUtils.contains(DARK_VALUES, originalValue)
+                ? blackColor
+                : originalValue;
     }
 
-    private static boolean anyEquals(int value, int... of) {
-        for (int v : of) if (value == v) return true;
-        return false;
+    public static void setHeaderGradient(ViewGroup viewGroup) {
+        viewGroup.getViewTreeObserver().addOnGlobalLayoutListener(() -> Utils.runOnMainThreadDelayed(() -> {
+            if (!(viewGroup.getChildAt(0) instanceof ViewGroup parentViewGroup))
+                return;
+            if (!(parentViewGroup.getChildAt(0) instanceof ImageView gradientView))
+                return;
+            if (headerGradient != null) {
+                gradientView.setForeground(headerGradient);
+            }
+        }, 0));
     }
 }
 

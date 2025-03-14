1b3ebe1a5806af4a09b07ef0ab5abcd72689a674
inotia00 <108592928+inotia00@users.noreply.github.com>
2025-03-14 18:33:31 +0900

    feat(YouTube): Replace with a fingerprint that supports a wider range of versions (..20.10)

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/fix/streamingdata/SpoofStreamingDataPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/fix/streamingdata/SpoofStreamingDataPatch.kt
@@ -19,6 +19,7 @@ import app.revanced.patches.youtube.utils.compatibility.Constants.COMPATIBLE_PAC
 import app.revanced.patches.youtube.utils.compatibility.Constants.YOUTUBE_PACKAGE_NAME
 import app.revanced.patches.youtube.utils.patch.PatchList.SPOOF_STREAMING_DATA
 import app.revanced.patches.youtube.utils.playservice.is_19_34_or_greater
+import app.revanced.patches.youtube.utils.playservice.is_20_10_or_greater
 import app.revanced.patches.youtube.utils.playservice.versionCheckPatch
 import app.revanced.patches.youtube.utils.request.buildRequestPatch
 import app.revanced.patches.youtube.utils.request.hookBuildRequest
@@ -331,6 +332,13 @@ val spoofStreamingDataPatch = bytecodePatch(
                 ONESIE_ENCRYPTION_FEATURE_FLAG,
                 "$EXTENSION_CLASS_DESCRIPTOR->skipResponseEncryption(Z)Z"
             )
+
+            if (is_20_10_or_greater) {
+                onesieEncryptionAlternativeFeatureFlagFingerprint.injectLiteralInstructionBooleanCall(
+                    ONESIE_ENCRYPTION_ALTERNATIVE_FEATURE_FLAG,
+                    "$EXTENSION_CLASS_DESCRIPTOR->skipResponseEncryption(Z)Z"
+                )
+            }
 
             settingArray += "SETTINGS: SKIP_RESPONSE_ENCRYPTION"
         }
